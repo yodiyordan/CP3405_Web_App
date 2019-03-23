1ff21bc5a6aeb1822e24d6285065596208e40a42
yodisy <yodi.yordan@my.jcu.edu.au>
2019-03-24 01:24:09 +0800

    Burndown and velocity chart added.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+import os
+
+IMAGES = os.path.join('static', 'images')
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = IMAGES
 
 
-@app.route('/webapp')
-def webapp():
-    return render_template('webapp.html')
+@app.route('/')
+@app.route('/index')
+def show_index():
+    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'burndown_and_velocity_chart.jpg')
+    return render_template("index.html", user_image=full_filename)
 
 
 if __name__ == "__main__":
