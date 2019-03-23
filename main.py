20701b61d410b0597f7dc8bc0dcaf521974eeb69
yodisy <yodi.yordan@my.jcu.edu.au>
2019-03-24 01:36:19 +0800

    Comments added.

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = IMAGES
 
 
+# For render_template pass in name of template and any variables needed
 @app.route('/')
 @app.route('/index')
+# To display images on the web page
 def show_index():
     full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'burndown_and_velocity_chart.jpg')
     return render_template("index.html", user_image=full_filename)
 
 
+# To specify how to run the app
 if __name__ == "__main__":
     app.run()
